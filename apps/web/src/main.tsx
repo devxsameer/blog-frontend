62f3fa75ac424a0e9a45cedb01b887d91eb13cd9
Sameer Ali <140313541+devxsameer@users.noreply.github.com>
2025-12-27 18:11:17 +0530

    chore(web): setup project structure and add import alias

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,9 @@
+// src/main.tsx
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div>Web Blog App</div>
+  </StrictMode>,
+)
---
+++ apps/web/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+
+function HomePage() {
+  return (
+    <div><h1>Home</h1><h2>Hello to everyone</h2></div>
+  )
+}
+
+export default HomePage
